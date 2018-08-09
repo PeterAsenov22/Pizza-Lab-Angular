c6cf02285b877c5b12a189d994ebe872455f08ae
PeterAsenov22 <peter12345@abv.bg>
2018-08-09 22:11:45 +0300

    Navigation and footer added

--- pizza-lab-angular-app/src/app/app.module.ts
+++ pizza-lab-angular-app/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
+import { SharedModule } from './components/shared/shared.module'
 
 import { AppComponent } from './app.component'
+import { RouterModule } from '../../node_modules/@angular/router'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ pizza-lab-angular-app/src/app/components/shared/index.ts
@@ -0,0 +1,7 @@
+import { NavigationComponent } from './navigation/navigation.component'
+import { FooterComponent } from './footer/footer.component'
+
+export const sharedComponents = [
+  NavigationComponent,
+  FooterComponent
+]
