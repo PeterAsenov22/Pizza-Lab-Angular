8d7e2f6ad2aef59b15d1bb8ffaa198af83075053
PeterAsenov22 <peter12345@abv.bg>
2018-08-10 18:28:57 +0300

    Home component added

--- pizza-lab-angular-app/src/app/app.module.ts
+++ pizza-lab-angular-app/src/app/app.module.ts
@@ -1,15 +1,20 @@
+import { AppRoutingModule } from './app.routing'
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
+import { RouterModule } from '../../node_modules/@angular/router'
 import { SharedModule } from './components/shared/shared.module'
 
+
 import { AppComponent } from './app.component'
-import { RouterModule } from '../../node_modules/@angular/router'
+import { HomeComponent } from './components/home/home.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     RouterModule,
     SharedModule
---
+++ pizza-lab-angular-app/src/app/app.routing.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+
+// Components
+import { HomeComponent } from './components/home/home.component'
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent }
+]
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
