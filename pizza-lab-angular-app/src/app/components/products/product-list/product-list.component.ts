8ea5be782c2181c511b274f5d4733a7c6fd97de5
PeterAsenov22 <peter12345@abv.bg>
2018-08-24 12:29:02 +0300

    Animations for product-list component added

--- pizza-lab-angular-app/src/app/components/products/product-list/product-list.component.ts
+++ pizza-lab-angular-app/src/app/components/products/product-list/product-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core'
 import { ProductModel } from '../models/ProductModel'
+import { animations } from './product-list.animations'
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss']
+  styleUrls: ['./product-list.component.scss'],
+  animations: animations
 })
 export class ProductListComponent {
   protected pageSize: number = 6
